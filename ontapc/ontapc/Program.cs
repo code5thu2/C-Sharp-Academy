7ec184299550742c156bbfce2be62d8d557e08a9
Lê Việt Anh <levietanhtdvp@gmail.com>
2021-02-23 14:03:34 +0700

    Add project files.

---
+++ ontapc/ontapc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ontapc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
